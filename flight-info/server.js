830b869a709ba5995cbf18125597793e29bfeb3b
ZevLipsky <zevilipsky@gmail.com>
2023-01-16 21:28:28 +0200

    uncomplete login

--- flight-info/server.js
+++ flight-info/server.js
@@ -1,103 +1,156 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const { pool } = require("./dbConfig");
+const bcrypt = require("bcrypt");
+const passport = require("passport");
+const flash = require("express-flash");
+const session = require("express-session");
+const app = express();
 const dotenv = require('dotenv');
-const path = require('path');
-const { ppid } = require('process');
-const {pool} = require("./dbConfig");
-const bcrypt = require('bcrypt');
-
 
 
 dotenv.config();
-const port = process.env.PORT 
+const port = process.env.PORT
 
-// app.use('/css', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/css')))
-// app.use('/js', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/js')))
+const initializePassport = require("./passportConfig");
 
-// let intialPath = path.join(__dirname, "public");
+initializePassport(passport);
 
-// app.use(express.json());
-// app.use(express.static(intialPath));
+// Middleware
 
-
-
-// app.get('/login', (req, res) => {
-//   res.sendFile(path.join(intialPath, "login.html"));
-// })
-
+// Parses details from a form
+app.use(express.urlencoded({ extended: false }));
 app.set("view engine", "ejs");
-app.use(express.urlencoded({extended: false}));
 
-app.get('/', (req,res) => {
-  res.render("index")
+app.use(
+  session({
+    // Key we want to keep secret which will encrypt all of our information
+    secret: 'secret',
+    // Should we resave our session variables if nothing has changes which we dont
+    resave: false,
+    // Save empty value if there is no vaue which we do not want to do
+    saveUninitialized: false
+  })
+);
+// Funtion inside passport which initializes passport
+app.use(passport.initialize());
+// Store our variables to be persisted across the whole session. Works with app.use(Session) above
+app.use(passport.session());
+app.use(flash());
+
+app.get("/", (req, res) => {
+  res.render("index");
 });
 
-app.get('/users/register', (req, res) =>{
-  res.render("register")
+app.get("/users/register", checkAuthenticated, (req, res) => {
+  res.render("register.ejs");
 });
 
-app.get('/users/login', (req, res) =>{
-  res.render("login")
+app.get("/users/login", checkAuthenticated, (req, res) => {
+  // flash sets a messages variable. passport sets the error message
+  // console.log(req.session.flash.error);
+  res.render("login.ejs");
 });
 
-app.get('/users/dashboard', (req, res) =>{
-  res.render("dashboard", {user: "Zev"})
+app.get("/users/dashboard", checkNotAuthenticated, (req, res) => {
+  console.log(req.isAuthenticated());
+  res.render("dashboard", { user: req.user.name });
 });
 
-app.post('/users/register', async (req,res) =>{
-  let { name, email, password, password2 } =req.body;
-  console.log({name,
+app.get("/users/logout", (req, res) => {
+  req.logout();
+  res.render("index", { message: "You have logged out successfully" });
+});
+
+app.post("/users/register", async (req, res) => {
+  let { name, email, password, password2 } = req.body;
+
+  let errors = [];
+
+  console.log({
+    name,
     email,
     password,
-    password2});
+    password2
+  });
 
-    let errors = [];
+  if (!name || !email || !password || !password2) {
+    errors.push({ message: "Please enter all fields" });
+  }
 
-    if(!name || !email || !password || !password2){
-      errors.push({message: "Please enter all fields"})
-    }
+  if (password.length < 6) {
+    errors.push({ message: "Password must be a least 6 characters long" });
+  }
 
-    if(password.length < 6){
-      errors.push({ message: "Password should be at least 6 charachters" });
+  if (password !== password2) {
+    errors.push({ message: "Passwords do not match" });
+  }
 
-    }
-
-    if(password != password2){
-      errors.push({message: "Passwords do not match!" });
-    }
-
-    if(errors.length > 0){
-      res.render("register", { errors });
-    }else{
-      //form validation has passed
-
-      let hashedPassword = await bcrypt.hash(password, 10);
-      console.log(hashedPassword);
-
-      pool.query(
-        `SELECT * FROM users
+  if (errors.length > 0) {
+    res.render("register", { errors, name, email, password, password2 });
+  } else {
+    hashedPassword = await bcrypt.hash(password, 10);
+    console.log(hashedPassword);
+    // Validation passed
+    pool.query(
+      `SELECT * FROM users
         WHERE email = $1`,
-         [email], 
-         (err, results) =>{
-          if (err) {
-            throw err;
-          }
-          console.log(results.rows);
+      [email],
+      (err, results) => {
+        if (err) {
+          console.log(err);
+        }
+        console.log(results.rows);
 
-          if(results.rows.length > 0){
-            errors.push({message: "Email is already registered"});
-            res.render('register', {errors})
-          }
+        if (results.rows.length > 0) {
+          return res.render("register", {
+            message: "Email already registered"
+          });
+        } else {
+          pool.query(
+            `INSERT INTO users (name, email, password)
+                VALUES ($1, $2, $3)
+                RETURNING id, password`,
+            [name, email, hashedPassword],
+            (err, results) => {
+              if (err) {
+                throw err;
+              }
+              console.log(results.rows);
+              req.flash("success_msg", "You are now registered. Please log in");
+              res.redirect("/users/login");
+            }
+          );
+        }
+      }
+    );
+  }
+});
 
-         }
+app.post(
+  "/users/login",
+  passport.authenticate("local", {
+    successRedirect: "/users/dashboard",
+    failureRedirect: "/users/login",
+    failureFlash: true
+  })
+);
 
-      )
-    }
+function checkAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return res.redirect("/users/dashboard");
+  }
+  next();
+}
 
+function checkNotAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect("/users/login");
+}
 
-
-})
 
 app.listen(port ||8080, () => {
   console.log(`Running on port ${port ||8080}`);
 })
+

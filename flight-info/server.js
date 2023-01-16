b2bf17cf8293ce93ea3c8efaad075b3e5371d22c
ZevLipsky <zevilipsky@gmail.com>
2023-01-16 14:08:39 +0200

    Login anf register pages

--- flight-info/server.js
+++ flight-info/server.js
@@ -2,20 +2,100 @@ const express = require('express')
 const app = express()
 const dotenv = require('dotenv');
 const path = require('path');
+const { ppid } = require('process');
+const {pool} = require("./dbConfig");
+const bcrypt = require('bcrypt');
+
 
 
 dotenv.config();
-let port = process.env.PORT
+const port = process.env.PORT 
 
-let intialPath = path.join(__dirname, "public");
+// app.use('/css', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/css')))
+// app.use('/js', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/js')))
 
-app.use(express.json());
-app.use(express.static(intialPath));
+// let intialPath = path.join(__dirname, "public");
+
+// app.use(express.json());
+// app.use(express.static(intialPath));
 
 
 
-app.get('/login', (req, res) => {
-  res.sendFile(path.join(intialPath, "login.html"));
+// app.get('/login', (req, res) => {
+//   res.sendFile(path.join(intialPath, "login.html"));
+// })
+
+app.set("view engine", "ejs");
+app.use(express.urlencoded({extended: false}));
+
+app.get('/', (req,res) => {
+  res.render("index")
+});
+
+app.get('/users/register', (req, res) =>{
+  res.render("register")
+});
+
+app.get('/users/login', (req, res) =>{
+  res.render("login")
+});
+
+app.get('/users/dashboard', (req, res) =>{
+  res.render("dashboard", {user: "Zev"})
+});
+
+app.post('/users/register', async (req,res) =>{
+  let { name, email, password, password2 } =req.body;
+  console.log({name,
+    email,
+    password,
+    password2});
+
+    let errors = [];
+
+    if(!name || !email || !password || !password2){
+      errors.push({message: "Please enter all fields"})
+    }
+
+    if(password.length < 6){
+      errors.push({ message: "Password should be at least 6 charachters" });
+
+    }
+
+    if(password != password2){
+      errors.push({message: "Passwords do not match!" });
+    }
+
+    if(errors.length > 0){
+      res.render("register", { errors });
+    }else{
+      //form validation has passed
+
+      let hashedPassword = await bcrypt.hash(password, 10);
+      console.log(hashedPassword);
+
+      pool.query(
+        `SELECT * FROM users
+        WHERE email = $1`,
+         [email], 
+         (err, results) =>{
+          if (err) {
+            throw err;
+          }
+          console.log(results.rows);
+
+          if(results.rows.length > 0){
+            errors.push({message: "Email is already registered"});
+            res.render('register', {errors})
+          }
+
+         }
+
+      )
+    }
+
+
+
 })
 
 app.listen(port ||8080, () => {
